fa2b46d410e7886afd8a1191badf077f82396992
Kim Nguyen <kimnivore@gmail.com>
2022-04-29 10:04:22 -0700

    added tooltip for scrolling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import Fade from 'react-reveal/Fade';
 import '../styles/App.css';
-import { Card, Image } from 'antd';
+import { Card, Image, Tooltip } from 'antd';
 
 const { Meta } = Card;
 
 const animateList = [ 
-    { title: 'Kirby, look at this.', description: "Black crayon on red construction paper", image: 'Kirby1' },
+    { title: 'Kirby, look at this.', description: "Blue crayon on red construction paper", image: 'Kirby1' },
     { title: 'Kirby, do not look at this.', description: "#2 pencil on pink construction paper", image: 'Kirby2' },
     { title: 'Kirby, I love you.', description: "Red crayon on blue construction paper", image: 'Kirby3' },
     { title: 'Kirby, take a picture of this.', description: "Red colored pencil on white construction paper", image: 'Kirby4' }
@@ -17,6 +17,7 @@ export default class ReactReveal extends Component {
         return (
             <Fragment>
                 {animateList.map((item, key) => (
+                    <Tooltip placement="right" title="scroll to view">
                     <div className="block" key={key}>
                         <Fade top>
                             <Card 
@@ -29,8 +30,10 @@ export default class ReactReveal extends Component {
                                     >
                                     <Meta title={item.title} description={item.description}/>
                             </Card>
-                        </Fade>
+                        
+                        </Fade>   
                     </div>
+                    </Tooltip>
                  ))}
             </Fragment>
         )
