2f8dffa72b21d4e93f326e665aee0914753f7f42
Kim Nguyen <kimnivore@gmail.com>
2022-05-03 15:47:16 -0700

    added gallery item #5

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,10 +6,11 @@ import { Card, Image, Tooltip } from 'antd';
 const { Meta } = Card;
 
 const animateList = [ 
-    { title: 'Kirby, look at this.', description: "Blue crayon on red construction paper", image: 'Kirby1' },
-    { title: 'Kirby, do not look at this.', description: "#2 pencil on pink construction paper", image: 'Kirby2' },
-    { title: 'Kirby, I love you.', description: "Red crayon on blue construction paper", image: 'Kirby3' },
-    { title: 'Kirby, take a picture of this.', description: "Red colored pencil on white construction paper", image: 'Kirby4' }
+    { title: 'Kirby, look at this', description: "Blue crayon on red construction paper", image: 'Kirby1' },
+    { title: 'Kirby, do not look at this', description: "#2 pencil on pink construction paper", image: 'Kirby2' },
+    { title: 'Kirby, I love you', description: "Red crayon on blue construction paper", image: 'Kirby3' },
+    { title: 'Kirby, take a picture of this', description: "Red colored pencil on white construction paper", image: 'Kirby4' },
+    { title: 'I ❤️ Kirby so much', description: "Black marker on post-it note with Avery Label #8160", image: 'Kirby5'},
 ];
 export default function Gallery() {
         return (
