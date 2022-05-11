92b2f4d9790baa26ae0f37b4256b06cf9240f50d
Kim Nguyen <kimnivore@gmail.com>
2022-05-11 09:44:25 -0700

    updated welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,9 +8,10 @@ export default function Welcome() {
                 <h1 >Welcome to the Keane x Kirby Collection</h1>
             </div> */}
             <div className='action'> 
-                <p>Mint a Kirby-themed NFT</p>   
-                <p>Check out Keane's art gallery</p>
-                <p>Play a flip card game</p>
+                <p>Welcome to Keane x Kirby Collection</p>
+                {/* <p>Mint a Kirby-themed NFT</p>    */}
+                {/* <p>Check out Keane's art gallery</p>
+                <p>Play a flip card game</p> */}
             </div>
         </>
     )
